9bb20f86526e9fa24f53b219d080eb89b603a6da
Shifa Samreen Savadatti <ShifaSamreen.S@BRILLIO.COM>
2022-05-06 17:46:11 +0530

    first commit

---
+++ src/Components/Home/home.tsx
@@ -0,0 +1,23 @@
+
+
+const Home = () => {
+    return(
+
+        <div>
+
+            <br /><br />
+
+            <h1 className="text-center">COVID VACCINATION APPLICATION</h1>
+            <p className="text-center">Get Vaccinted Go Immune And Fight Covid-19</p>
+
+            <br /><br />
+
+            <img className="mx-auto d-block" src="https://media.istockphoto.com/vectors/inspirational-positive-quote-about-novel-coronavirus-covid19-pandemic-vector-id1216214778?k=6&m=1216214778&s=170667a&w=0&h=fEXk6JYm69cvje-I5xmiFJ_9sI36gCS2dQ3B_7kY7HQ=" alt="vaccining" width="500" height="300" />
+
+        </div>
+
+    )
+
+}
+
+export default Home;
